2c43d4d21aaf9ef072be8110e40a3f1a72a08aaf
Domnigg20 <100387265+Domnigg20@users.noreply.github.com>
2024-05-01 17:47:02 +0100

    Update chapter9.sql

--- chapter9.sql
+++ chapter9.sql
@@ -1,7 +1,11 @@
 
 ----Answer to section 9 in Practical SQL, a beginner' guide.
 ----how many of the plants in the table process meat, and how many process poultry
-SELECT 
-    SUM(CASE WHEN activities ILIKE '%Meat Processing%' THEN 1 ELSE 0 END) AS meat_plants,
-    SUM(CASE WHEN activities ILIKE '%Poultry Processing%' THEN 1 ELSE 0 END) AS poultry_plants
-FROM public.meat_poultry_egg_inspect;
+SELECT COUNT(activities) 
+FROM public.meat_poultry_egg_inspect mpi
+WHERE EXISTS (
+    SELECT * 
+    FROM meat_poultry_egg_inspect_temp temp
+    WHERE temp.activities = mpi.activities
+    AND (temp.activities ILIKE '%Meat Processing%' )
+);
